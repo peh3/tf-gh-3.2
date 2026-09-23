0417a23f2faad2c34c24113cb6e8463b02be1e3a
peh3 <tk_peh@hotmail.com>
2026-09-23 20:59:42 +0800

    update terraform fmt

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 terraform {
   backend "s3" {
-    bucket       = "sctp-tfstate-ce13"
-    key          = "tk/tf-gh-3.2"
-    region       = "us-east-1"
+    bucket = "sctp-tfstate-ce13"
+    key    = "tk/tf-gh-3.2"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-   region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "workshop" {
