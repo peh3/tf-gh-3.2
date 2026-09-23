53939c0503eb3b0905cfadcf34e75c5985470995
peh3 <tk_peh@hotmail.com>
2026-09-23 20:55:44 +0800

    Adding new s3 bucket

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket       = "sctp-tfstate-ce13"
+    key          = "tk/tf-gh-3.2"
+    region       = "us-east-1"
+  }
+}
+
+provider "aws" {
+   region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "workshop" {
+  bucket_prefix = "tk-tf-gh-3.2"
+  tags = {
+    Purpose = "github-actions-workshop"
+  }
+}
