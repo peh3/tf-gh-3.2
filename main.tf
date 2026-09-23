8245735ef5b8bc5b1cdb56392eca1d9e2766fc2b
peh3 <tk_peh@hotmail.com>
2026-09-23 21:36:29 +0800

    update checkov

--- main.tf
+++ main.tf
@@ -20,6 +20,12 @@ provider "aws" {
 resource "aws_s3_bucket" "workshop" {
   bucket_prefix = "tk-tf-gh-3.2"
   #checkov:skip=CKV_AWS_145:Ensure that S3 buckets are encrypted with KMS by default
+  #checkov:CKV_AWS_18:"Ensure the S3 bucket has access logging enabled"
+  #checkov:CKV_AWS_144:"Ensure that S3 bucket has cross-region replication enabled"
+  #checkov:CKV2_AWS_6:"Ensure that S3 bucket has a Public Access block"
+  #checkov:CKV2_AWS_62:"Ensure S3 buckets should have event notifications enabled"
+  #checkov:CKV_AWS_21:"Ensure all data stored in the S3 bucket have versioning enabled"
+  #checkov:CKV2_AWS_61:"Ensure that an S3 bucket has a lifecycle configuration"
 
   tags = {
     Purpose = "github-actions-workshop"
